a13f72a5d93f9f0e6c8bdefb5e306ef2cb8974fe
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-03-13 17:53:26 -0400

    Update App.java
    user prompt added

--- SimpleCalculator/src/App.java
+++ SimpleCalculator/src/App.java
@@ -10,5 +10,16 @@ public class App {
     //main method
     public static void main(String[] args) throws Exception {
         
+
+        //prompt for the first number
+        System.out.println("Enter the first number: ");
+
+        //prompts the user to enter a value for the second number
+        System.out.println("Enter the second number: ");
+
+
+
+
+
     }
 }
