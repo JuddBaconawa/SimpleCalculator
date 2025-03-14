393205c3b1fd6f698ad7df1f2c3c7cb50dfc5244
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-03-14 12:35:57 -0400

    App.java and Calculations.java modified
    App.java -
    prompts added which gives the user the formula before plugging in values or the arithmetic symbol + prompt which ask the user to use an arithmetic symbol.
    
    Calculations.java -
    subtract method added to the Calculations class.

--- SimpleCalculator/src/Calculations.java
+++ SimpleCalculator/src/Calculations.java
@@ -10,5 +10,9 @@ public class Calculations {
 	public double add(double firstNumber, double secondNumber) {
 		return firstNumber + secondNumber;
 	}
+
+	public double subtract(double firstNumber, double secondNumber) {
+		return firstNumber - secondNumber;
+	}
 	
 }
