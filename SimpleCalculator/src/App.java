cb81c0170015a9bf9cbec16e76ca09df680089a6
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-03-16 22:48:54 -0400

    App.java and UserService.java created and edit
    App.java - prompts. scanner, and value info for the calculator remove due to new class where it would be.
    
    UserService.java class created to hide the backend function and to make the code cleaner
    
    -trying it because its what chat gpt said when i inputted my code and asked it to make it better. i should have finished it first but yea

--- SimpleCalculator/src/App.java
+++ SimpleCalculator/src/App.java
@@ -11,34 +11,10 @@ public class App {
     //main method
     public static void main(String[] args) throws Exception {
         
-        //scanner initialized
-        Scanner scanner = new Scanner(System.in);
-
-        //promts the user how the numbers and arithmetic expression is used within a formula
-        System.out.println("Enter the first number + arithmetic symbol + second number = result");
-
-        //prompt for the first number
-        System.out.println("Enter the first number: ");
-        double firstNumber = scanner.nextDouble();
-
-        System.out.println("Enter the arithmetic operator: ");
-        char arithmeticSymbol = scanner.next().charAt(0);
-
-        //prompts the user to enter a value for the second number
-        System.out.println("Enter the second number: ");
-        double secondNumber = scanner.nextDouble();
-
-
-        if (firstNumber.isEmpty && secondNumber.isEmpty) {
-            System.out.println("Empty input! Try typing a number(s)");
-        } else {
-
-            
-
-        }
+        UserService userService = new UserService();
+        userService.startCalculator();
 
 
 
-        scanner.close();
     }
 }
---
+++ SimpleCalculator/src/UserService.java
@@ -0,0 +1,18 @@
+//package
+
+
+//imports
+import java.util.Scanner;
+
+//class
+public class UserService {
+
+	private Scanner scanner;
+	private Calculations calculations;
+
+	public UserService() {
+		this.scanner = new Scanner(System.in);
+		this.calculations = new Calculations();
+	}
+	
+}
