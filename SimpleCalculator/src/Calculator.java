06fd1e519baa45ff1fe5c5434cf2f4e453d31913
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-08-15 09:14:07 -0700

    Update Calculator.java
    Calculator java updated - frame for the calculator itself is added + JTextField for the numbers to show up on as a display view

---
+++ SimpleCalculator/src/Calculator.java
@@ -0,0 +1,30 @@
+// PACKAGES
+
+// IMPORTS
+import java.awt.event.ActionListener;
+
+
+
+//main
+public class Calculator {
+
+    Calculator() {
+
+    }
+
+    public static void main(String[] args) {
+
+        // Main window for the calculator GUI
+        JFrame calculatorFrame = new JFrame("Calculator");
+        calculatorFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        calculatorFrame.setSize(400, 300);
+        calculatorFrame.setLayout(BoxLayout);
+
+        // Creating a JTextField to display the digits when buttons are pressed
+        JTextField displayField = new JTextField();
+        displayField.setEditable
+
+        
+
+    }
+}
