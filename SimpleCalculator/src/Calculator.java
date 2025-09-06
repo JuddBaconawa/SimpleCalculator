aa13a56c29f67b7f56c87020e3a1c789c0082329
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-09-06 19:15:39 -0400

    Update Calculator.java
    Old code delted,
    
    new imports added, Calculator inherits from actionlisteners
    
    number buttons and functions buttons added
     textfield added

--- SimpleCalculator/src/Calculator.java
+++ SimpleCalculator/src/Calculator.java
@@ -1,30 +1,50 @@
 // PACKAGES
 
 // IMPORTS
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 
 
 //main
-public class Calculator {
+public class Calculator implements ActionListener{
+
+    JFrame frame;
+    JTextField textfield;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+
+    JButton addButton, subButton, multButton, divButton;
+    JButton decButton, equButton, delButton, clrButton;
+    JPanel panel;
+
+    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+
+    double num1=0,num2=0,result=0;
+    char operator;
 
     Calculator() {
+
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(450, 550);
+        frame.setLayout(null);
+
+        
+
+        frame.setVisible(true);
 
     }
 
     public static void main(String[] args) {
-
-        // Main window for the calculator GUI
-        JFrame calculatorFrame = new JFrame("Calculator");
-        calculatorFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        calculatorFrame.setSize(400, 300);
-        calculatorFrame.setLayout(BoxLayout);
-
-        // Creating a JTextField to display the digits when buttons are pressed
-        JTextField displayField = new JTextField();
-        displayField.setEditable
-
         
+        Calculator calc = new Calculator();
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
     }
 }
