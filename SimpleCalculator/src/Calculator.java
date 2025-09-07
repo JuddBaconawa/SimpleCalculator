f6d3e348f2d8f7051ee0a6f1f7778bcc91524cf8
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-09-06 20:45:41 -0400

    Update Calculator.java
    comments added + spacing issues resolved
    
    textfield added: setbounds, fonts, editable set to false, then added into the frame.
    
    comments added to display how the code worked from the last update

--- SimpleCalculator/src/Calculator.java
+++ SimpleCalculator/src/Calculator.java
@@ -10,20 +10,25 @@ import java.awt.event.*;
 //main
 public class Calculator implements ActionListener{
 
+    // GUI components: Buttons for numbers and operations + text field
     JFrame frame;
-    JTextField textfield;
+    JTextField textField;
     JButton[] numberButtons = new JButton[10];
     JButton[] functionButtons = new JButton[8];
 
+    // Function buttons: add, subtract, multiply, divide, decimal, equals, delete, clear
     JButton addButton, subButton, multButton, divButton;
     JButton decButton, equButton, delButton, clrButton;
     JPanel panel;
 
+    // Creates and uses a custom font
     Font myFont = new Font("Ink Free", Font.BOLD, 30);
 
-    double num1=0,num2=0,result=0;
+    // Variables to store the numbers
+    double num1=0, num2=0, result=0;
     char operator;
 
+    // Constructor for the Class of the same name
     Calculator() {
 
         frame = new JFrame("Calculator");
@@ -31,8 +36,13 @@ public class Calculator implements ActionListener{
         frame.setSize(450, 550);
         frame.setLayout(null);
 
-        
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setFont(myFont);
+        textField.setEditable(false);
 
+
+        frame.add(textField);
         frame.setVisible(true);
 
     }
