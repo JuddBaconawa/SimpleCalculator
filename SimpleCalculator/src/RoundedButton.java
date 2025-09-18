e8ea55fe8dc37e729340e858e221e6baa03d7660
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-09-17 23:04:15 -0400

    Update RoundedButton.java
    imports added for swing and awt.
    
    @Override added for adding radius to the buttons +

--- SimpleCalculator/src/RoundedButton.java
+++ SimpleCalculator/src/RoundedButton.java
@@ -1,6 +1,10 @@
 // PACKAGES
 
 // IMPORTS
+import javax.swing.JButton;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 
 // Main  class
@@ -17,5 +21,28 @@ public class RoundedButton extends JButton {
         setContentAreaFilled(false);
         setFocusPainted(false);
 
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // Fill rounded rectangle with button background color
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+
+        //Draw Button Text
+        super.paintComponent(g);
+        g2.dispose();
+    }
+
+    @Override
+    protected void paintBorder(Graphics g) {
+        Graphics2D g2 =(Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getForeground());
+        g2.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, radius, radius);
+        g2.dispose();
     }
 }
