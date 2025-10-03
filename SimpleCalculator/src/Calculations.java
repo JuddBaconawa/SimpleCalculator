5457349db9d085e0b23076e41e37ec217f52914d
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-10-03 18:31:33 -0400

    Update Calculations.java
    Calculations class deleted

--- SimpleCalculator/src/Calculations.java
+++ SimpleCalculator/src/Calculations.java
@@ -7,20 +7,6 @@
 //Calculation class
 public class Calculations {
 
-	public double add(double firstNumber, double secondNumber) {
-		return firstNumber + secondNumber;
-	}
 
-	public double subtract(double firstNumber, double secondNumber) {
-		return firstNumber - secondNumber;
-	}
-
-	public double divide(double firstNumber, double secondNumber) {
-		return firstNumber / secondNumber;
-	}
-
-	public double multiply(double firstNumber, double secondNumber) {
-		return firstNumber * secondNumber;
-	}
 	
 }
