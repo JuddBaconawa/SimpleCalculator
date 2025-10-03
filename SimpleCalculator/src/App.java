3c08a6e345d2651acfbc0467d5603c34a862ddca
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-10-03 18:43:57 -0400

    Update App.java
    comments added on what it does/ how it works

--- SimpleCalculator/src/App.java
+++ SimpleCalculator/src/App.java
@@ -8,7 +8,7 @@ public class App {
     //main method
     public static void main(String[] args) throws Exception {
 
-        new Calculator();
+        new Calculator();   // Creates and launches the calculator GUI
 
 
 
